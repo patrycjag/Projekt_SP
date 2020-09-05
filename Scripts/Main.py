4158d1f754f485c7a8d8fdaf540edfc4a9b9de3d
Łukasz Kępka <lukaszkepka97@gmail.com>
2020-09-05 20:06:54 +0200

    Added data for tensors and changed to v1 in code

--- Scripts/Main.py
+++ Scripts/Main.py
@@ -1,5 +1,6 @@
 # TensorFlow and tf.keras
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
 from tensorflow import keras
 
 # Helper libraries
@@ -7,3 +8,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 print(tf.__version__)
+X_1 = tf.placeholder(tf.float32, name = "X_1")
+X_2 = tf.placeholder(tf.float32, name = "X_2")
+
+multiply = tf.multiply(X_1, X_2, name = "multiply")
+
+with tf.Session() as session:
+    result = session.run(multiply, feed_dict={X_1:[1,2,3], X_2:[4,5,6]})
+    print(result)
